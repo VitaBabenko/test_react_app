b32a0b1cf2ba2543411b15ccc1abf1fdb759ec6d
VitaBabenko <babenkovita1988@gmail.com>
2023-04-27 22:47:45 +0300

    changed favicon

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets_app__react">
+    <BrowserRouter basename="/tweets_react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
