94007c2b4c2de029e56b06b5eb0e70936f39ea19
VitaBabenko <babenkovita1988@gmail.com>
2023-04-27 21:08:16 +0300

    changed basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test_react_app">
+    <BrowserRouter basename="/tweets_app__react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
