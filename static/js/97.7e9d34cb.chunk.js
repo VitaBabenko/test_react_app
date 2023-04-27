b89364e895b0a8cfa68db93ad1da7a65a8c447c8
VitaBabenko <110460253+VitaBabenko@users.noreply.github.com>
2023-04-27 18:03:35 +0000

    Deploying to gh-pages from @ VitaBabenko/tweets_app__react@42254f504bed598394c6b61fd549429235b2cb89 🚀

---
+++ static/js/97.7e9d34cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktweets_app_react=self.webpackChunktweets_app_react||[]).push([[97],{97:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});var r,c=t(168),a=t(562).ZP.h1(r||(r=(0,c.Z)(["\n  color: #471ca9;\n  text-transform: uppercase;\n  text-align: center;\n  margin-top: 50px;\n"]))),u=t(184),p=function(){return(0,u.jsx)(a,{children:"Hello!"})},s=function(){return(0,u.jsx)(p,{})}}}]);
+//# sourceMappingURL=97.7e9d34cb.chunk.js.map
--- static/js/97.8e7e81a2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_react_app=self.webpackChunktest_react_app||[]).push([[97],{97:function(n,t,e){e.r(t),e.d(t,{default:function(){return s}});var r,c=e(168),a=e(562).ZP.h1(r||(r=(0,c.Z)(["\n  color: #471ca9;\n  text-transform: uppercase;\n  text-align: center;\n  margin-top: 50px;\n"]))),u=e(184),p=function(){return(0,u.jsx)(a,{children:"Hello!"})},s=function(){return(0,u.jsx)(p,{})}}}]);
-//# sourceMappingURL=97.8e7e81a2.chunk.js.map
